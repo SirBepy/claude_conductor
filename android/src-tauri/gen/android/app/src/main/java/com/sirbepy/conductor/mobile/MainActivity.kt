232cb35aed60b637a91256c7f5dd0ffc04d6a758
SirBepy <tecnomon99@gmail.com>
2026-08-05 19:17:25 +0200

    REVERT: extend TauriActivity, not WryActivity - PluginManager.onActivityCreate is required for the barcode scanner plugin to bind

--- android/src-tauri/gen/android/app/src/main/java/com/sirbepy/conductor/mobile/MainActivity.kt
+++ android/src-tauri/gen/android/app/src/main/java/com/sirbepy/conductor/mobile/MainActivity.kt
@@ -3,9 +3,7 @@ package com.sirbepy.conductor.mobile
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
-// WryActivity (generated/WryActivity.kt) is the real base class - same
-// package, no import needed. TauriActivity never existed in this version.
-class MainActivity : WryActivity() {
+class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
